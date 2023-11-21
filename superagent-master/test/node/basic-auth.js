6d40a4f165e8595294ad19ff0d2a70d5456e44ce
Arthur Prado <arthurpradoo5@gmail.com>
2023-11-20 23:21:20 -0300

    Inicio do cód

---
+++ superagent-master/test/node/basic-auth.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const URL = require('url');
+const request = require('../support/client');
+const getSetup = require('../support/setup');
+
+describe('Basic auth', () => {
+  let setup;
+  let base;
+
+  before(async () => {
+    setup = await getSetup();
+    base = setup.uri;
+  });
+
+  describe('when credentials are present in url', () => {
+    it('should set Authorization', (done) => {
+      const new_url = URL.parse(base);
+      new_url.auth = 'tobi:learnboost';
+      new_url.pathname = '/basic-auth';
+
+      request.get(URL.format(new_url)).end((error, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+    });
+  });
+
+  describe('req.auth(user, pass)', () => {
+    it('should set Authorization', (done) => {
+      request
+        .get(`${base}/basic-auth`)
+        .auth('tobi', 'learnboost')
+        .end((error, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+  });
+
+  describe('req.auth(user + ":" + pass)', () => {
+    it('should set authorization', (done) => {
+      request
+        .get(`${base}/basic-auth/again`)
+        .auth('tobi')
+        .end((error, res) => {
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+  });
+});
